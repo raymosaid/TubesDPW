f13ac445a589dbfe620feb5d422431fe91bd32cf
Briyaw <yawbriyaw@gmail.com>
2023-06-21 00:53:43 +0700

    Update index.php

--- application/views/index.php
+++ application/views/index.php
@@ -46,7 +46,7 @@
       <a href="index.php" class="logo d-flex align-items-center me-auto me-lg-0">
         <!-- Uncomment the line below if you also wish to use an image logo -->
         <!-- <img src="assets/img/logo.png" alt=""> -->
-        <h1>RF Nasgor<span>.</span></h1>
+        <h1>RF Nasgorr<span>.</span></h1>
       </a>
 
       <nav id="navbar" class="navbar">
