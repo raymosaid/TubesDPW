71f79d72db3047eb17197c5e2473b7b43fbb53cc
raymosaid <raymosaid@users.noreply.github.com>
2023-06-21 13:34:41 +0700

    sql

---
+++ sql/t_details.sql
@@ -0,0 +1,3 @@
+INSERT INTO `t_details` VALUES (0, '123456', '2023-6-20 23:35:48', 'nasi goreng', '20000', '5', '100000');
+INSERT INTO `t_details` VALUES (1, '123456', '2023-6-20 23:35:48', 'jus alpukat', '20000', '5', '100000');
+INSERT INTO `t_details` VALUES (2, '654321', '2023-6-21 00:45:15', 'set menu a', '10000', '1', '100000');
---
+++ sql/t_pemesanan.sql
@@ -0,0 +1,2 @@
+INSERT INTO `t_pemesanan` VALUES (1, '2023-6-20 23:35:48', '123456', 'John', 200000);
+INSERT INTO `t_pemesanan` VALUES (2, '2023-6-21 00:45:15', '654321', 'David', 100000);
